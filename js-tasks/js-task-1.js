90463185bd69bda1f53356701fd2b7fd4736d8df
marjanaL <liylukm@gmail.com>
2026-01-20 09:29:19 +0000

    JS task 1

---
+++ js-tasks/js-task-1.js
@@ -0,0 +1,7 @@
+//www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+

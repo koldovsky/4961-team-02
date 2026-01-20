08a2f3539cf86380cef8ae1f547810b0d957a363
marjanaL <liylukm@gmail.com>
2026-01-20 09:50:20 +0000

    3

--- js-tasks/js-task-1.js
+++ js-tasks/js-task-1.js
@@ -10,3 +10,8 @@ function makeNegative(num) {
   return num > 0 ? -num : num;
 }
 
+//www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+
+function move(position, roll) {
+  return position + roll * 2;
+}

1a75a9611471eb3d00506318410525935dfb76aa
Bogdan Salnyk <111270103+HAt0r1@users.noreply.github.com>
2026-01-20 18:25:25 +0200

    Add multiple functions for various tasks

--- js-tasks/js-task-1.js
+++ js-tasks/js-task-1.js
@@ -53,3 +53,62 @@ function litres(time) {
 function lovefunc(flower1, flower2){
   return flower1%2 === 0 && flower2%2 !==0 || flower1%2 !== 0 && flower2%2 ===0 ? true : false;
 }
+
+//7
+//www.codewars.com/kata/58ca658cc0d6401f2700045f/train/javascript
+
+function findMultiples(integer, limit) {
+  const result = [];
+  for(let i = integer; i <= limit; i += integer) {
+    result.push(i);
+  }
+  return result;
+}
+
+//8
+//www.codewars.com/kata/5174a4c0f2769dd8b1000003/train/javascript
+
+function solution(nums){
+  if((typeof nums === "object" && Array.isArray(nums)) && nums.length !== 0) {
+    return nums.sort((cur, next) => cur - next);
+  }
+  return [];
+}
+
+//9
+//www.codewars.com/kata/57e92e91b63b6cbac20001e5/train/javascript
+
+function dutyFree(normPrice, discount, hol){
+  const finalPrice = normPrice * (discount / 100);
+  return Math.floor(hol / finalPrice);
+}
+
+//10
+//www.codewars.com/kata/5734c38da41454b7f700106e/train/javascript
+
+function onlyOne(...args) {
+  if(args.length === 0) return false;
+  const trueArray = args.filter(val => val === true);
+  return trueArray.length === 1 ? true : false;
+}
+
+//11
+//www.codewars.com/kata/590e03aef55cab099a0002e8/train/javascript
+
+function incrementer(nums) {
+  return nums.map((value, index) => (value + index + 1) % 10);
+}
+
+//12
+//www.codewars.com/kata/581214d54624a8232100005f/train/javascript
+
+function matrix(array) {
+    for(let i = 0; i < array.length; i++) {
+      if(array[i][i] < 0) {
+        array[i][i] = 0;
+      } else {
+        array[i][i] = 1;
+      }
+    }
+    return array;
+}

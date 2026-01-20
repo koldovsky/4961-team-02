291f72e91fd4eb88138474c8daa40c4b708e91a7
marjanaL <liylukm@gmail.com>
2026-01-20 09:47:22 +0000

    2

--- js-tasks/js-task-1.js
+++ js-tasks/js-task-1.js
@@ -4,4 +4,9 @@ function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 }
 
+//www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+
+function makeNegative(num) {
+  return num > 0 ? -num : num;
+}
 

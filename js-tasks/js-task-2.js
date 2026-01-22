ab36f0f0c175c2de13ea3b18ca1c464395449592
Bogdan Salnyk <111270103+HAt0r1@users.noreply.github.com>
2026-01-22 20:28:51 +0200

    Add solution for advanced tasks

--- js-tasks/js-task-2.js
+++ js-tasks/js-task-2.js
@@ -11,6 +11,17 @@ function DNAtoRNA(dna) {
   return dna.replaceAll("T", "U");
 }
 
+// Bohdan Salnyk
+const DNAtoRNA = dna => {
+  let result = [...dna].reduce((acc, symbol) => {
+    if(symbol === "T") {
+        return [...acc, "U"];
+    }
+    return [...acc, symbol];
+  }, []);
+  return result.join("");
+}
+
 //3
 //www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
 var min = function (list) {
@@ -33,6 +44,11 @@ var max = function (list) {
   return max;
 }
 
+// Bohdan Salnyk
+const min = list => Math.min(...list);
+
+const max = list => Math.max(...list);
+
 //4
 //www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
 function min(arr, toReturn) {
@@ -44,3 +60,18 @@ function min(arr, toReturn) {
   }
   return toReturn === "value" ? min : arr.indexOf(min);
 }
+
+// Bohdan Salnyk
+const min = (arr, toReturn) => { 
+    const minValue = Math.min(...arr);
+    const indexOfMinValue = arr.indexOf(minValue);
+    return toReturn === "value" ? minValue : indexOfMinValue;
+}
+
+//5
+//www.codewars.com/kata/53ee5429ba190077850011d4/train/javascript
+const doubleInteger = i => i * 2;
+
+//6
+//www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
+const twiceAsOld = (dadYearsOld, sonYearsOld) => Math.abs(dadYearsOld - (sonYearsOld * 2));

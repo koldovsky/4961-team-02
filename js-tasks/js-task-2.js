a1cf48b17b094cf3ed5dad903f8e1958881d2d45
Bogdan Salnyk <111270103+HAt0r1@users.noreply.github.com>
2026-01-23 17:08:04 +0200

    Add getRealFloor function to handle floor logic

--- js-tasks/js-task-2.js
+++ js-tasks/js-task-2.js
@@ -90,3 +90,10 @@ const past = (h,m,s) => {
 //9
 //www.codewars.com/kata/5545f109004975ea66000086/train/javascript
 const isDivisible = (n, x, y) => n % x === 0 && n % y === 0;
+
+//10
+//www.codewars.com/kata/574b3b1599d8f897470018f6/train/javascript
+const getRealFloor = n => {
+  if(n <= 0) return n;
+  return n >= 13 ? n - 2 : n - 1;
+}

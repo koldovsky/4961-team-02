d8a2ccec9e7154ad4f22f73b6e4a4f98d4abbf44
Bogdan Salnyk <111270103+HAt0r1@users.noreply.github.com>
2026-01-26 15:49:31 +0200

    Add circleCircumference function for circle radius

--- js-tasks/js-task-3.js
+++ js-tasks/js-task-3.js
@@ -1,4 +1,5 @@
 //1. Geometry Basics: Circle Circumference in 2D
+//www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript
 const circleCircumference = circle => {
   return 2 * (Math.PI * circle.radius);
 }

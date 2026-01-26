654e2587306d48476c056feeb277d62f0a045462
IhorT <magorych1998@gmail.com>
2026-01-26 15:14:45 +0200

    Empty js-task-3 file added

---
+++ js-tasks/js-task-3.js
@@ -0,0 +1,7 @@
+//1. Geometry Basics: Circle Circumference in 2D
+
+//2. Training JS #12: loop statement --for..in and for..of
+
+//3. Understanding closures - the basics
+
+//4. Fun with ES6 Classes #2 - Animals and Inheritance
